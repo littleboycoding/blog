3fca71aa89479df73cc6a55cd254a4f8cb1aff6d
Thanawat Yodnil <littleboycoding@gmail.com>
2020-02-13 19:06:43 +0700

    Change the whole site to use SWR instead for better performance

--- pages/blog/[title].js
+++ pages/blog/[title].js
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
 import Markdown from "react-markdown";
 import Link from "next/link";
+import useSWR from "swr";
+import { useRouter } from "next/router";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretSquareLeft } from "@fortawesome/free-solid-svg-icons";
 
@@ -8,52 +10,45 @@ const Container = dynamic(() => import("../../components/container"), {
   loading: () => <h1>{". . ."}</h1>
 });
 
+async function fetchData(url) {
+  return await fetch(url).then(res => res.json());
+}
+
 function Blog(props) {
-  const title = props.content.split("\n")[0];
-  return (
-    <Container title={title}>
-      <style jsx>{`
-        span:hover {
-          text-decoration: underline !important;
-          cursor: pointer;
-        }
-      `}</style>
-      <Link href="/">
-        <p>
-          <FontAwesomeIcon icon={faCaretSquareLeft} /> <span>ย้อนกลับ</span>
-        </p>
-      </Link>
-      <style jsx global>{`
-        .markdown p img {
-          width: 100%;
-          max-height: 450px;
-          object-fit: contain;
-          background-color: #eee;
-        }
-      `}</style>
-      <Markdown className="markdown">{`# ${props.content}`}</Markdown>
-    </Container>
+  const router = useRouter();
+  const { data, error } = useSWR(
+    "/api/fetchContent?title=" + router.query.title,
+    fetchData
   );
-}
-
-export async function unstable_getStaticPaths() {
-  const fs = require("fs");
-  const path = require("path");
-  const paths = [];
-  const markdownArray = fs.readdirSync("./markdown");
-  const basenamedMD = markdownArray.map(markdown => path.parse(markdown).name);
-  basenamedMD.forEach(markdown => paths.push({ params: { title: markdown } }));
-
-  return paths;
-}
-
-export async function unstable_getStaticProps(context) {
-  const fs = require("fs");
-  const params = context.params;
-  const title = params.title;
-  const resultMarkdown = fs.readFileSync(`./markdown/${title}.md`, "utf8");
-
-  return { props: { content: resultMarkdown } };
+  if (error) return "ไม่พบเนื้อหาที่คุณกำลังมองหา กรุณาลองใหม่อีกครั้งภายหลัง";
+  if (!data) return null;
+  if (data) {
+    const title = data.content.split("\n")[0];
+    return (
+      <Container title={title}>
+        <style jsx>{`
+          span:hover {
+            text-decoration: underline !important;
+            cursor: pointer;
+          }
+        `}</style>
+        <Link href="/">
+          <p>
+            <FontAwesomeIcon icon={faCaretSquareLeft} /> <span>ย้อนกลับ</span>
+          </p>
+        </Link>
+        <style jsx global>{`
+          .markdown p img {
+            width: 100%;
+            max-height: 450px;
+            object-fit: contain;
+            background-color: #eee;
+          }
+        `}</style>
+        <Markdown className="markdown">{`# ${data.content}`}</Markdown>
+      </Container>
+    );
+  }
 }
 
 export default Blog;
--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,46 @@
 import dynamic from "next/dynamic";
 import Link from "next/link";
-import fetch from "isomorphic-unfetch";
+import useSWR from "swr";
 import React, { Fragment } from "react";
 
 const Container = dynamic(() => import("../components/container"), {
   loading: () => <h1>{". . ."}</h1>
 });
 
-function BlogList(props) {
-  console.log(props.blog);
-  const list = props.blog.map(blogArray => (
-    <Fragment key={blogArray.fileName}>
-      <style jsx>{`
-        li:hover {
-          text-decoration: underline !important;
-          cursor: pointer;
-        }
-      `}</style>
-      <Link href={`/blog/[title]`} as={`/blog/${blogArray.fileName}`}>
-        <li>{blogArray.title}</li>
-      </Link>
-    </Fragment>
-  ));
-
-  return <ul>{list}</ul>;
+async function fetchData(url) {
+  return await fetch(url).then(res => res.json());
 }
 
-function Index(props) {
+function BlogList() {
+  const { data, error } = useSWR("/api/fetchBlog", fetchData);
+  if (error) return "เกิดข้อผิดพลาดในขณะกำลังดาวน์โหลดข้อมูล";
+  if (!data) return "กำลังโหลดข้อมูล";
+  if (data) {
+    const list = data.blog.map(blogArray => (
+      <Fragment key={blogArray.fileName}>
+        <style jsx>{`
+          li:hover {
+            text-decoration: underline !important;
+            cursor: pointer;
+          }
+        `}</style>
+        <Link href={`/blog/[title]`} as={`/blog/${blogArray.fileName}`}>
+          <li>{blogArray.title}</li>
+        </Link>
+      </Fragment>
+    ));
+
+    return <ul>{list}</ul>;
+  }
+}
+
+function Index() {
   return (
     <Container title="หน้าหลัก">
       <h1>📰 Blog</h1>
-      <BlogList blog={props.blog} />
+      <BlogList />
     </Container>
   );
 }
-
-Index.getInitialProps = async () => {
-  const res = await fetch(
-    `http://localhost:${process.env.PORT || 3000}/api/fetchBlog`
-  ).then(res => res.json());
-
-  return { blog: await res.blog };
-};
-
-//export async function unstable_getStaticProps() {
-//const fs = require("fs");
-//const path = require("path");
-//const markdownArray = fs.readdirSync("./markdown");
-//const contentList = markdownArray.map(markdown => ({
-//fileName: path.parse(markdown).name,
-//title: fs.readFileSync(`./markdown/${markdown}`, "utf8").split("\n")[0]
-//}));
-//
-//return {
-//props: { blog: contentList }
-//};
-//}
 
 export default Index;
