42ad7a492bb4f3ac0845af3f932bccc350bb5114
Thanawat Yodnil <littleboycoding@gmail.com>
2020-03-22 18:21:32 +0700

    Fixed thumnbnail doesn't load bug

--- pages/_app.js
+++ pages/_app.js
@@ -46,9 +46,8 @@ export default ({ Component, pageProps }) => {
           key="og:image"
           property="og:image"
           content={
-            pageProps.thumbnail
-              ? baseURL + pageProps.thumbnail
-              : "https://www.gravatar.com/avatar/81f506d45aad1acc94b8d6a64bc6a448?s=1000"
+            pageProps.thumbnail ? pageProps.thumbnail.search("http") === 0 ? pageProps.thumbnail :
+              baseURL + pageProps.thumbnail : "https://www.gravatar.com/avatar/81f506d45aad1acc94b8d6a64bc6a448?s=1000"
           }
         />
         <meta
